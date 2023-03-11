b92723538508c30bf0ad8fa828266f02f44a4929
iboard <iboard@qq.com>
2023-03-11 14:39:25 +0800

    feat: '修改文件命链接，修改视频播放背景'

--- src/pages/extraction-tool/router/index.js
+++ src/pages/extraction-tool/router/index.js
@@ -4,40 +4,35 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 const routes = [
   {
-    path: '/index',
-    name: 'index',
-    meta: { title: '随机数' },
-    component: () => import('../views/index.vue'),
+    path: '*',
+    name: '',
+    redirect: '/random',
   },
   {
     path: '/random',
     name: 'random',
-    meta: { title: '抽签助手' },
-    component: () => import('../views/random.vue'),
+    meta: { title: '随机数' },
+    component: () => import('../views/index.vue'),
   },
-  {
-    path: '*',
-    name: '',
-    redirect: '/home',
-  },
-  {
-    path: '/home',
-    name: 'home',
-    meta: { title: '随机抽签首页' },
-    component: () => import('../views/home/index.vue'),
-  },
-  {
-    path: '/grouping',
-    name: 'grouping',
-    meta: { title: '分组' },
-    component: () => import('../views/home/grouping.vue'),
-  },
-  {
-    path: '/random-number',
-    name: 'random-number',
-    meta: { title: '选号' },
-    component: () => import('../views/home/random-number.vue'),
-  },
+
+  // {
+  //   path: '/home',
+  //   name: 'home',
+  //   meta: { title: '随机抽签首页' },
+  //   component: () => import('../views/home/index.vue'),
+  // },
+  // {
+  //   path: '/grouping',
+  //   name: 'grouping',
+  //   meta: { title: '分组' },
+  //   component: () => import('../views/home/grouping.vue'),
+  // },
+  // {
+  //   path: '/random-number',
+  //   name: 'random-number',
+  //   meta: { title: '选号' },
+  //   component: () => import('../views/home/random-number.vue'),
+  // },
 ]
 
 const router = new VueRouter({
--- src/pages/extraction-tool/views/index.vue
+++ src/pages/extraction-tool/views/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content">
+  <div :class="isPlayVideo ? 'content' : 'content content-background'">
     <video
       class="video-box"
       v-if="isPlayVideo"
